e84e238a51f173f17a96c11f98d3249d513de9d5
Jhojan Stiwer <stiwercc@gmail.com>
2023-11-15 22:02:34 -0500

    updateButton function already working

--- 11 - Custom Video Player/script.js
+++ 11 - Custom Video Player/script.js
@@ -1,6 +1,11 @@
 /*Get our elements */
 const player = document.querySelector(".player");
 const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
 
 /* Build out functions */
 
@@ -9,6 +14,15 @@ function togglePlay() {
   video[method]();
 }
 
+function updateButton() {
+    const icon = this.paused ?  '🙈' : '🙊';
+    toggle.textContent = icon;
+}
+
 /* Hook up the event*/
 
 video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+
+toggle.addEventListener("click", togglePlay);
