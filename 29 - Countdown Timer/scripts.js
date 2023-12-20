5ac60a108f984db45d2b951fe53af17dd59a713d
Jhojan Stiwer <stiwercc@gmail.com>
2023-12-20 09:57:12 -0500

    loop if for check if should stop it

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -1,8 +1,17 @@
+let countdown;
+
 function timer(seconds) {
     const now = Date.now()
     const then = now + seconds * 1000
-    setInterval(() => {
-        const secondsLeft =Math.round((then - Date.now()) / 1000);
+
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        // Check if we should stop it
+        if (secondsLeft <= 0) {
+            clearInterval(countdown);
+            return;
+        }
+        //Display it
         console.log(secondsLeft)
     }, 1000);
 }
